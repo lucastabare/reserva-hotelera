e38301869f42881b75ba92b50481ec0a9c684fcd
Lucas Tabare <lucas.tabare@gmail.com>
2022-05-11 18:17:43 -0300

    Creando rutas de la app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import { Counter } from "./features/counter/Counter";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import SearchPage from "./components/SearchPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      <Home />
-      <SearchPage />
-    </div>
+      <Routes>
+        <Route exact path="/search" element={<SearchPage />} />
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
